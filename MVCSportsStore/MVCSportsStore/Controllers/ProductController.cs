b601aeac9a8ae6dd2e5db224e61d5a0c48e4b505
ZHLiYang <41778953+ZHLiYang@users.noreply.github.com>
2018-09-13 18:50:08 +0800

    Revert "Revert "up""
    This reverts commit 63a33d4d62846f44171a3e8988eb132ed521ed06.

---
+++ MVCSportsStore/MVCSportsStore/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Domain.Abstract;
+using Domain.Entities;
+using MVCSportsStore.Models;
+
+namespace MVCSportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+        private IProductRepository repository;
+        public ProductController(IProductRepository productRepository)
+        {
+            this.repository = productRepository;
+        }
+        public int PageSize = 4;
+        public ViewResult List(string category, int page = 1)
+        {
+            ProductListViewModel model = new ProductListViewModel
+            {
+                Products = repository.Products
+                .Where(p => category == null || p.Category == category)
+                .OrderBy(p => p.ProductID)
+                .Skip((page - 1) * PageSize)
+                .Take(PageSize),
+                paginginfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems =category==null? repository.Products.Count():
+                    repository.Products.Where(p => category==p.Category).Count()
+                },
+                CurrentCategory=category
+            };
+            return View(model);
+        }
+
+    }
+}
---
+++ MVCSportsStore/MVCSportsStore/Models/ProductListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain.Entities;
+
+
+namespace MVCSportsStore.Models
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo paginginfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
